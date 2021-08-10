1ae73b33e3b32f1c33222b97ee2432bd274bacb6
LyriCaGitHubber <pf.munich.developer@gmail.com>
2021-08-10 18:29:19 +0200

    Create server with express framework

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,17 @@
+import express from 'express';
 import { readCredentials } from './utils/credentials';
 
-readCredentials();
+const app = express();
+const port = 3000;
+
+app.get('/api/credentials', async (_req, res) => {
+  res.status(200).send(await readCredentials());
+});
+
+app.get('/', (_req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
