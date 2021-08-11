b49823b9561ad8679cad24807eb15db5424e9400
LyriCaGitHubber <pf.munich.developer@gmail.com>
2021-08-11 12:03:36 +0200

    ✨ Create route to get single credential

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,28 @@
-import express from 'express';
-import { readCredentials } from './utils/credentials';
+import express, { response } from 'express';
+import { getCredential, readCredentials } from './utils/credentials';
 
 const app = express();
 const port = 3000;
 
 app.get('/api/credentials', async (_req, res) => {
-  res.status(200).send(await readCredentials());
+  try {
+    const credentials = await readCredentials();
+    res.status(200).json(credentials);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send(`Internal server error.`);
+  }
+});
+
+app.get('/api/credentials/:service', async (request, response) => {
+  const { service } = request.params;
+  try {
+    const credential = await getCredential(service);
+    response.status(200).json(credential);
+  } catch (error) {
+    console.error(error);
+    response.status(404).send(`Could not find service: ${service}`);
+  }
 });
 
 app.get('/', (_req, res) => {
