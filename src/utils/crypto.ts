bda23c20d5f73db6a40e0f5c9feb7195ed47bf1c
LyriCaGitHubber <pf.munich.developer@gmail.com>
2021-08-13 11:56:07 +0200

    Create function to decrypt credential and add key argument to encryptCredential function

--- src/utils/crypto.ts
+++ src/utils/crypto.ts
@@ -1,10 +1,13 @@
 import CryptoJS from 'crypto-js';
 import { Credential } from '../types';
 
-export function encryptCredential(credential: Credential): Credential {
+export function encryptCredential(
+  credential: Credential,
+  key: string
+): Credential {
   const enryptedPassword = CryptoJS.TripleDES.encrypt(
     credential.password,
-    'MySecretkey'
+    key
   ).toString();
 
   const encryptedCredential = {
@@ -12,4 +15,19 @@ export function encryptCredential(credential: Credential): Credential {
     password: enryptedPassword,
   };
   return encryptedCredential;
+}
+
+export function decryptCredential(
+  credential: Credential,
+  key: string
+): Credential {
+  const decryptedPassword = CryptoJS.TripleDES.decrypt(
+    credential.password,
+    key
+  ).toString(CryptoJS.enc.Utf8);
+  const decryptedCredential = {
+    ...credential,
+    password: decryptedPassword,
+  };
+  return decryptedCredential;
 }
